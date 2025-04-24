ece92ddf2431456e98469b9407624b5d0521e333
Drew Allred <drew.e.allred@gmail.com>
2025-04-24 08:07:38 -0400

    pfp

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -11,7 +11,7 @@ import ScrambleText from "../ScrambleText"
 import ResumeCard from "../ResumeCard"
 
 // assets
-import ProfilePic from "../../assets/Images/Profile pic.png"
+import ProfilePic from "../../assets/Images/Profile Pic.png"
 import Icon from "../Icon"
 import githubIcon from "../../assets/Icon/github.svg"
 import linkedinIcon from "../../assets/Icon/linkedin.svg"
