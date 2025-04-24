c1ec6b3b15dd53ef9dd2fbda106ac41d4fed4186
Drew Allred <drew.e.allred@gmail.com>
2025-04-24 07:39:05 -0400

    initial push

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react"
+import "./style.css"
+import BackgroundLines from "../BackgroundLines"
+import WorkCard from "../WorkCard"
+import ScrambleText from "../ScrambleText"
+import ParaWriting from "../ParaWriting"
+import { motion, useAnimation } from "framer-motion"
+import { useInView } from "react-intersection-observer"
+
+import work1 from "../../assets/Images/work1.png"
+import work2 from "../../assets/Images/work2.png"
+import work3 from "../../assets/Images/work3.png"
+
+export default function Projects() {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+  const [hasAnimated, setHasAnimated] = useState(false)
+
+  const handleComplete = () => {
+    setHasAnimated(true)
+  }
+
+  useEffect(() => {
+    // Start animation when the component is in view
+    if (inView && !hasAnimated) {
+      controls.start("visible")
+    }
+  }, [inView, controls])
+
+  const works = [
+    {
+      client: "client name",
+      year: "2023",
+      img: work1,
+      title: "Revamping Global Lifestyle Brand.",
+      detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      client: "client name",
+      year: "2023",
+      img: work2,
+      title: "Next generation Web Management System.",
+      detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      client: "client name",
+      year: "2023",
+      img: work3,
+      title: "Share your Life Stories. Generationally.",
+      detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+  ]
+
+  const opacityVariant = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  }
+
+  return (
+    <section ref={ref} className="projects" id="projects">
+      <BackgroundLines />
+      <div className="background--glow"></div>
+
+      <div className="projects--grid">
+        <motion.div initial="hidden" animate={controls} variants={opacityVariant} transition={{ duration: 1, delay: 0.5 }} className="projects--grid--title">
+          <h3 className="theme--text">
+            <ScrambleText shuffle delay={0.5}>
+              03
+            </ScrambleText>{" "}
+            <span className="hash">{"//"}</span>{" "}
+            <ScrambleText shuffle delay={0.5}>
+              Expertise
+            </ScrambleText>
+          </h3>
+        </motion.div>
+
+        <div className="projects--grid--content">
+          <div className="projects--grid--content--heading">
+            <h2>
+              <ParaWriting stagger={0.08} text={"My "} sec={"Works"} />
+            </h2>
+          </div>
+          <div className="projects--grid--content--works">
+            {works.map((item, index) => {
+              return (
+                <WorkCard
+                  item={item}
+                  key={index}
+                  // delay={0.1 * index + 1}
+                  // controls={controls}
+                />
+              )
+            })}
+          </div>
+        </div>
+
+        <motion.div initial="hidden" animate={controls} variants={opacityVariant} transition={{ duration: 1, delay: 1 }} onAnimationComplete={() => handleComplete()} className="projects--grid--detail">
+          <p className="theme--detail">
+            <ScrambleText delay={1}>Discover a curated portfolio of projects where each line of code tells a story of problem-solving, creativity, and technical finesse.</ScrambleText>
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  )
+}
